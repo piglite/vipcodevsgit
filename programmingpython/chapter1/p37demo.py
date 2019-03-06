30aaa5584674baf20a86c2558709a4e44b02720f
piglite <piglite@vip.sina.com>
2019-03-06 10:30:06 +0800

    update

--- programmingpython/chapter1/p37demo.py
+++ programmingpython/chapter1/p37demo.py
@@ -1,17 +1,18 @@
-bob = {'name':'Bob Smith','age':24,'pay':6000,'job':'dev'}
-sue = {'name':'Sue Jones','age':32,'pay':15000,'job':'hdw'}
-tom = {'name':'Tom Pages','age':36,'pay':26000,'job':'mgr'}
+bob = {'name': 'Bob Smith', 'age': 24, 'pay': 6000, 'job': 'dev'}
+sue = {'name': 'Sue Jones', 'age': 32, 'pay': 15000, 'job': 'hdw'}
+tom = {'name': 'Tom Pages', 'age': 36, 'pay': 26000, 'job': 'mgr'}
 
 import pickle
-for (k,v) in [('bob',bob),('sue',sue),('tom',tom)]:
-    filewriter = open(k + '.pkl','wb')
-    pickle.dump(v,filewriter)
+
+for (k, v) in [('bob', bob), ('sue', sue), ('tom', tom)]:
+    filewriter = open(k + '.pkl', 'wb')
+    pickle.dump(v, filewriter)
     filewriter.close()
 
 import glob
+
 for filename in glob.glob('*.pkl'):
-    reader = open(filename,'rb')
+    reader = open(filename, 'rb')
     value = pickle.load(reader)
-    print(filename,'==>',value)
+    print(filename, '==>', value)
     reader.close()
-    
